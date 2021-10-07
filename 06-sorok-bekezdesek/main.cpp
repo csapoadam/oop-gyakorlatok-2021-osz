3697b64d0bc5668e5ce7dabe7dbfc55cbf93fbe3
csapoadam <csapo.adam@gmail.com>
2021-10-07 19:01:17 +0200

    sorok szavakra bontasa

--- 06-sorok-bekezdesek/main.cpp
+++ 06-sorok-bekezdesek/main.cpp
@@ -39,24 +39,38 @@ public:
 	}
 };
 
-class Bekezdes {};
+class Bekezdes {
+	std::vector<Sor> sorok;
+public:
+	void addText(const std::string& text) {
+		int inxOfNextSpace = 0;
+		int inxOfPreviousSpace = -1;
+		bool stop = false;
+
+		while (!stop) {
+			inxOfNextSpace = text.find(" ", inxOfPreviousSpace + 1);
+			std::string nextSzo = text.substr(inxOfPreviousSpace + 1,
+				inxOfNextSpace - inxOfPreviousSpace - 1);
+			inxOfPreviousSpace = inxOfNextSpace;
+
+			if (nextSzo == "" || inxOfNextSpace == -1) {
+				stop = true;
+			}
+			if (nextSzo.length() > 0) {
+				// adjuk hozza az utolso sorhoz!!
+				std::cout << "hozzaadva!" << std::endl;
+			}
+			std::cout << "kovetkezo szo: ." << nextSzo << "." << std::endl;
+		}
+	}
+	void print() {}
+};
 
 int main()
 {
-	Szo v1("valami");
-	Szo v2("furcsa");
-	Szo v3("es");
-	Szo v4("megmagyarazhatatlan");
-	
-	Sor sor1;
-	sor1.addSzo(v1);
-	sor1.addSzo(v2);
-	sor1.addSzo(v3);
-	sor1.addSzo(v4);
-	sor1.print();
-	/*Bekezdes b;
+	Bekezdes b;
 	b.addText("This is a test szoveg.");
 	b.addText("Please, please add this text and do not forget to create new lines in the process.");
 	b.addText("Keep going with creating new lines as long as the previous line has more than 75 characters");
-	b.print();*/
+	b.print();
 }
