7cdac4a0605d46d57b3293e223703fc3d51a6d15
csapoadam <csapo.adam@gmail.com>
2021-10-14 18:17:13 +0200

    elso plecsniig jok vagyunk!

--- 07-katonak-kituntetesek/Katonak.cpp
+++ 07-katonak-kituntetesek/Katonak.cpp
@@ -1,13 +1,21 @@
 #include "katonak.h"
 
 Katona::Katona(const std::string& name, const std::string& rank, int yearOfBirth) :
-	nev(name), rang(rank), szuletesiEv(yearOfBirth) {
+	nev(name), rang(rank), szuletesiEv(yearOfBirth), elsoPlecsni(nullptr) {
 }
 
-void Katona::addPlecsni(const std::string& nev, int year) {}
+void Katona::addPlecsni(const std::string& nev, int year) {
+	KituntetesNode* newnode = new KituntetesNode(nev, year);
+	if (!elsoPlecsni) {
+		elsoPlecsni = newnode;
+	}
+}
 
 void Katona::print() {
 	std::cout << "\t" << nev << " (" << rang << " - " << szuletesiEv << ")";
 	std::cout << std::endl;
+	if (elsoPlecsni) {
+		elsoPlecsni->print();
+	}
 	// itt meg a plecsniket ki kell iratni!
 }
--- 07-katonak-kituntetesek/kituntetesek.h
+++ 07-katonak-kituntetesek/kituntetesek.h
@@ -9,7 +9,18 @@ public:
 		nev(name), evszam(year)
 	{}
 	void print() {
-		std::cout << "\tKituntetes: " << nev << " ("
+		std::cout << "\t\t" << nev << " ("
 			<< evszam << ")" << std::endl;
 	}
+};
+
+class KituntetesNode {
+	Kituntetes value;
+	KituntetesNode* next;
+public:
+	KituntetesNode(const std::string& kitNev, int yearOfKit) :
+		next(nullptr), value(kitNev, yearOfKit)
+	{}
+	void setNext(KituntetesNode* nxt) { next = nxt; }
+	void print() { value.print(); }
 };
