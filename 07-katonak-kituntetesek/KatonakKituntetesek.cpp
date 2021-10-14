f2900580a3a2aac218cee958e901d41979105895
csapoadam <csapo.adam@gmail.com>
2021-10-14 19:15:12 +0200

    visual studioban mem.szivargas detektalasa

--- 07-katonak-kituntetesek/KatonakKituntetesek.cpp
+++ 07-katonak-kituntetesek/KatonakKituntetesek.cpp
@@ -3,16 +3,42 @@
 #include "kituntetesek.h"
 #include "katonak.h"
 
+#include "windows.h"
+#define _CRTDBG_MAP_ALLOC //to get more details
+#include <stdlib.h>  
+#include <crtdbg.h>   //for malloc and free
+
 int main()
 {
-	Katona x("Eros Pista", "Hadnagy", 1977);
-	Katona y("Harcos Helga", "Ezredes", 1979);
+	_CrtMemState sOld;
+	_CrtMemState sNew;
+	_CrtMemState sDiff;
+	_CrtMemCheckpoint(&sOld); //take a snapshot
+
+	Katona* x = new Katona("Eros Pista", "Hadnagy", 1977);
+	Katona* y = new Katona("Harcos Helga", "Ezredes", 1979);
 
 	// az addPlecsni() metodus hozza letre a kituntetes objektumokat!
-	x.addPlecsni("Arany Akarmi Kereszt", 1997);
-	x.addPlecsni("Arany Felso Kereszt", 2002);
-	y.addPlecsni("Arany Facan a Legjobb!", 2005);
+	x->addPlecsni("Arany Akarmi Kereszt", 1997);
+	x->addPlecsni("Arany Felso Kereszt", 2002);
+	y->addPlecsni("Arany Facan a Legjobb!", 2005);
 
-	x.print();
-	y.print();
+	x->print();
+	y->print();
+
+	delete x;
+	delete y;
+	
+	_CrtMemCheckpoint(&sNew); //take a snapshot 
+	if (_CrtMemDifference(&sDiff, &sOld, &sNew)) // if there is a difference
+	{
+		OutputDebugString(L"-----------_CrtMemDumpStatistics ---------");
+		_CrtMemDumpStatistics(&sDiff);
+		OutputDebugString(L"-----------_CrtMemDumpAllObjectsSince ---------");
+		_CrtMemDumpAllObjectsSince(&sOld);
+		OutputDebugString(L"-----------_CrtDumpMemoryLeaks ---------");
+		_CrtDumpMemoryLeaks();
+	}
+
+	return 0;
 }
