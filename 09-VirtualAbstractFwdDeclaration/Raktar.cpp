73d0a377b88338bc5fc647ff555b864a50638839
csapoadam <csapo.adam@gmail.com>
2021-10-28 18:27:11 +0200

    hierarchia absztrakt osztallyal + fwd deklaracio

---
+++ 09-VirtualAbstractFwdDeclaration/Raktar.cpp
@@ -0,0 +1,43 @@
+// Raktar.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// Raktar osztaly:
+// Termek tipusu pointereket tarol el
+// Termek legyen absztrakt osztaly!
+// mindegyik termekhez szamon tart mennyiseget
+// A termek lehet garancialis v. csereszavatos (csere garancialis?)
+// ezen belul legyen TV, Mobiltelefon, Laptop
+// minden termek "tudja", hogy melyik
+// ... raktarbol szarmazik
+// igy ha a termeket "visszakuldik", ez megteheto
+
+
+#include <iostream>
+
+#include "termekek.h"
+
+int main()
+{
+    Raktar r;
+    Laptop l1("ASUS TUF Gaming", 2020, 5);
+    Laptop l2("Lenovo Legion Y520", 2018, 3);
+    Mobilkeszulek m1("Xperia Hypersuper", 2019, 4);
+    l1.print();
+    l2.print();
+    m1.print();
+    l1.javit(&r);
+    l2.javit(&r);
+    m1.javit(&r);
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ 09-VirtualAbstractFwdDeclaration/termekek.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Raktar; // forward deklaracio!
+// ez azert fontos, mert a Termek interfesze (javit()) hivatkozik
+// a Raktar tipusra, de a fordito nem tudja hogy ilyen tipus letezik!
+// itt viszont nem lehet kifejteni a Raktar osztalyt, mert abban lesznek
+// Termek*-ok is, mivel egy vektorban tarolnia kell a Termekek cimeit
+
+// termeknek legyen:
+// neve
+// elettartama (evekben merve)
+// gyartasi eve 
+// ha az elettartam letelik, minden evben 50% esellyel
+// elromlik
+// legyen benne egy flag, hogy elromlott 
+class Termek {
+	std::string nev;
+	int elettartam;
+	int gyartasiEv;
+	bool elromlott;
+public:
+	Termek(const std::string& nm, int ge, int et)
+		: nev(nm), elettartam(et), gyartasiEv(ge), elromlott(false) {}
+	virtual void print() {
+		// virtual, mert akar felul is definialhato.
+		// DE: nem muszaj feluldefinialni, mivel itt is elvben minden info
+		// rendelkezesre all
+		std::cout << nev << " (gyartas eve: " << gyartasiEv << "), elromlott?" << elromlott << std::endl;
+	}
+	virtual void javit(Raktar*) = 0; // pure virtual method! -> abstract class
+	// ezt, hogy javit() itt nem tudjuk definialni, mert nem tudjuk, hogy
+	// javithato-e a termek (garancialis? vagy cserelik rogton?)
+};
+
+class Raktar {
+	std::vector<Termek*> termekek;
+};
+
+class Garancialis : public Termek {
+public:
+	Garancialis(const std::string nm, int gyartasiev, int elettartam) :
+		Termek(nm, gyartasiev, elettartam) {}
+	void javit(Raktar*) override {
+		std::cout << "A termeket megprobaljuk javitani" << std::endl;
+	}
+};
+
+class Csereszavatos : public Termek {
+public:
+	Csereszavatos(const std::string nm, int gyartasiev, int elettartam) :
+		Termek(nm, gyartasiev, elettartam) {}
+	void javit(Raktar*) override {
+		std::cout << "A termek nem javithato, de csereljuk!" << std::endl;
+	}
+};
+
+class Laptop : public Garancialis {
+public:
+	Laptop(const std::string nm, int gyartasiev, int elettartam) :
+		Garancialis(nm, gyartasiev, elettartam) {}
+};
+
+class Mobilkeszulek : public Csereszavatos {
+public:
+	Mobilkeszulek(const std::string nm, int gyartasiev, int elettartam) :
+		Csereszavatos(nm, gyartasiev, elettartam) {}
+};
