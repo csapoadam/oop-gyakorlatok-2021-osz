4ffa0ab6f348aedf62c97359ff20e2dcc7a0368e
csapoadam <csapo.adam@gmail.com>
2021-11-25 18:47:45 +0100

    tamado es vedekezo ero. tamadas

--- 10-TablaJatek/babuk.h
+++ 10-TablaJatek/babuk.h
@@ -15,6 +15,14 @@ public:
 	void freeze() { isFrozen = true; }
 	void unfreeze() { isFrozen = false; }
 	bool isStateFrozen() { return isFrozen; }
+	virtual double getTamadoEro() = 0;
+	virtual double getVedekezoEro() = 0;
+	bool megtamad(Babu* vedekezo) {
+		// akkor igaz, ha a tamado nyer; kulonben hamis
+		std::cout << "\t" << char_rep() << " megtamadta " << vedekezo->char_rep();
+		std::cout << "-t ... hajjaj!" << std::endl;
+		return this->getTamadoEro() > vedekezo->getVedekezoEro();
+	}
 };
 
 class Aligator : public Babu {
@@ -22,6 +30,8 @@ public:
 	char char_rep() override {
 		return 'A';
 	}
+	double getTamadoEro() { return 0.8; }
+	double getVedekezoEro() { return 0.6; }
 };
 
 class Csirke : public Babu {
@@ -29,6 +39,8 @@ public:
 	char char_rep() override {
 		return 'C';
 	}
+	double getTamadoEro() { return 0.2; }
+	double getVedekezoEro() { return 0.2; }
 };
 
 class Ember : public Babu {
@@ -36,4 +48,6 @@ public:
 	char char_rep() override {
 		return 'E';
 	}
+	double getTamadoEro() { return 0.8; }
+	double getVedekezoEro() { return 0.7; }
 };
