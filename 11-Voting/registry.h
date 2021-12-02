60326daf0a5c17a4d94ba87f5b68f8ef629fde3c
csapoadam <csapo.adam@gmail.com>
2021-12-02 19:08:48 +0100

    referencia kell! - igaz, nemcsak itt

--- 11-Voting/registry.h
+++ 11-Voting/registry.h
@@ -18,7 +18,7 @@ public:
 	void initializeValasztas(Valasztas v) {
 		valasztas = v;
 	}
-	void szavaz(int id, Valasztas v, int jelolt) {
+	void szavaz(int id, Valasztas& v, int jelolt) {
 		// eloszor ellenorizni kene, hogy:
 		// - letezik-e ilyen szavazo?
 		// - nem szavazott-e mar?
